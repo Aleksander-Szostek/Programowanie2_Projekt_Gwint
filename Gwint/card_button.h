7318b923fbbd113264ca9fffd0ab34e9aac4019c
kajiex <wardzioch24@gmail.com>
2026-05-14 21:37:12 +0200

    dynamiczne karty
    dodałem klase button card, która dodaje przycisk o parametrach karty i w mainwindow dodałem tymczasowy tester w postaci przycisku, który dodaje kartę aby zobaczyć czy działa

---
+++ Gwint/card_button.h
@@ -0,0 +1,20 @@
+#ifndef CARD_BUTTON_H
+#define CARD_BUTTON_H
+
+#include "karta.h"
+#include <QPushButton>
+
+class Card_Button : public QPushButton
+{
+    Q_OBJECT
+public:
+    explicit Card_Button(karta* daneKarty, QWidget *parent=nullptr); //przekazuje wskaźnik danych karty
+
+    void refresh(); //do aktualizacji wyglądu karty, np siły po jakiś efektach
+
+    karta* getCardData() {return k_dane;}
+private:
+    karta* k_dane;
+};
+
+#endif // CARD_BUTTON_H
--- Gwint/mainwindow.cpp
+++ Gwint/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include "card_loader.h"
 #include "deck_loader.h"
 #include "deck_saver.h"
+#include "card_button.h"
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -10,6 +11,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //testwalem wyrównanie od lewej do prawej przy dodawaniu zamiast tak jak jest bazowo
+    // ui->layout_reki->layout()->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+
+    // ui->layout_reki->layout()->setSpacing(20);
     int test = 42;
 
     card_loader zaladuj;
@@ -19,8 +24,9 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->Plansza->setStyleSheet(
         "border-image: url(:/gwont_plansza_templatka.png) 0 0 0 0 stretch stretch;"
-        );
-
+    );
+    qDebug() << QPixmap(":/plansza.jpg").isNull();
+    qDebug() << QPixmap(":/gwont_plansza_templatka.png").isNull();
 
     //    ui->label->setText(karci.nazwa);
     //else
@@ -39,3 +45,31 @@ void MainWindow::on_verticalSlider_sliderMoved(int position)
 {
 
 }
+
+
+void MainWindow::on_button_dobierz_clicked()
+{
+    //tworze testwa karte czy się dodają poprawnie
+    karta* nowaKartaDane = new karta();
+    nowaKartaDane->setName("Bogata Kurcze Piechota");
+    nowaKartaDane->setSilaBaz(100);
+    nowaKartaDane->updateSila(false, false, 0); //updatuje sile na 1
+
+    nowaKartaDane->setLeg(true);
+
+    Card_Button *przyciskKarty = new Card_Button(nowaKartaDane, this);
+    przyciskKarty->getCardData();
+    przyciskKarty->refresh();
+    ui->layout_reki->addWidget(przyciskKarty);
+
+
+    connect(przyciskKarty, &Card_Button::clicked, this, [=]() {
+        qDebug() << "Wybrano karte: " << nowaKartaDane->getNazwa();
+
+
+        //usuwanie karty po kliknieciu, tak jakbyś ja przeniósł na planszę
+        ui->layout_reki->removeWidget(przyciskKarty);
+        przyciskKarty->hide();
+    });
+}
+
